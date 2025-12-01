f1f0603702fe0dea3ac88d974680cd6145ac0101
J-RAY3E <81426829+J-RAY3E@users.noreply.github.com>
2025-12-01 10:49:15 +0300

    updates req
    update req

---
+++ src/main/java/com/example/pointchecker/util/HibernateUtil.java
@@ -0,0 +1,29 @@
+package com.example.pointchecker.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+
+    private static final EntityManagerFactory entityManagerFactory;
+
+    static {
+        try {
+            entityManagerFactory = Persistence.createEntityManagerFactory("PointCheckerPU");
+        } catch (Throwable ex) {
+            System.err.println("Initial EntityManagerFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
+
+    public static void shutdown() {
+        if (entityManagerFactory != null) {
+            entityManagerFactory.close();
+        }
+    }
+}
